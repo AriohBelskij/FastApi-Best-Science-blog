5fdd54b32116a67c008c4101d132ba549600c84c
arioh <eugeneyakim4@gmail.com>
2023-10-08 02:21:11 +0300

    updated alembic.ini and env.py

--- migration/env.py
+++ migration/env.py
@@ -1,28 +1,28 @@
 import os
 import sys
 from logging.config import fileConfig
-
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
 
-from app.core.database import Base
-from app.core.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
+from app.core.database import DATABASE_URL
+
+from app.models.base import Base
+
 
 
 sys.path.append(os.path.join(sys.path[0], "app"))
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
 section = config.config_ini_section
-config.set_section_option(section, "DB_HOST", DB_HOST)
-config.set_section_option(section, "DB_USER", DB_USER)
-config.set_section_option(section, "DB_NAME", DB_NAME)
-config.set_section_option(section, "DB_PORT", DB_PORT)
-config.set_section_option(section, "DB_PASS", DB_PASS)
+config.set_section_option(section, "DB_LINK", DATABASE_URL)
+
+
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -33,7 +33,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = [Base.metadata]
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
